d23010408c05a0cfca2e1e6a7593528d27c6fae7
koleber <koleber@estaxi.ru>
2018-05-23 11:03:18 +0600

    Init commit

---
+++ app/src/main/java/com/bignerdranch/android/myapplication/SunsetFragment.java
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        SunsetFragment fragment = new SunsetFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+        return view;
+    }
+}
